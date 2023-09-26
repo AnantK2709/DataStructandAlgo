88cd2b47caadeb83cb9cdb2afead5dfe8fc48389
AnantK2709 <anant009@e.ntu.edu.sg>
2023-09-26 15:30:22 +0800

    small modi

---
+++ arrays/tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+ int main(){
+     int row,col;
+     cin>>row>>col;
+     for(int i=row;i>=1;i--)
+     {
+         for(int j=1;j<=i;j++)
+         {
+             cout<<"*";
+         }
+         cout<<endl;
+     }
+ }
