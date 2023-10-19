f737b56c989886a623400ab28dac8fc150acbb1b
AnantK2709 <anant009@e.ntu.edu.sg>
2023-10-19 16:35:06 +0800

    heapsort

--- arrays/heapsort.cpp
+++ arrays/heapsort.cpp
@@ -1,28 +1,44 @@
 #include <bits/stdc++.h>>
 using namespace std;
 
-
-
+void Heapifying(int arr[], int n, int i)
+{
+    int largest = i; // initialise largest as root - default
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+    //if left is greater than root
+    if(arr[l]>arr[largest] && l<n) // the left child should exist, otherwise it might take a garbage value
+    {
+        largest = l;
+    }
+    if(arr[r]>arr[largest] && r<n)
+    {
+        largest = r;            
+    }
+}
 
 void HeapSort(int arr[], int n)
 {
-    ConstructHeap(arr,);
-    for(int i=n;i>=1;i--)
+    for (int i = n / 2 - 1; i >= 0; i--) // heapify onlt the nodes which are not leaf nodes
+    {                                    // therfore it starts with n/2-1 ->nodes with childs
+        Heapifying(arr, n, i);
+    }
+    for (int i = n; i >= 1; i--)
     {
         int curMax = getMax(H);
         deleteMax(H);
-        arr[i] = currMax;    
+        arr[i] = currMax;
     }
 }
 int main()
 {
     int n;
-    cout<<"Enter the size of the array";
-    cin>>n;
+    cout << "Enter the size of the array";
+    cin >> n;
     int arr[n];
-    Heapsort(arr,n);
-    for(int i=0;i<n;i++)
+    Heapsort(arr, n);
+    for (int i = 0; i < n; i++)
     {
-        cout<<arr[i];
+        cout << arr[i];
     }
 }
